9dd07b3286582b1d86fbed69b975212ac52a8217
Lucky-1300 <luckykumari42774@gmail.com>
2025-11-28 15:02:40 +0530

    done

---
+++ script.js
@@ -0,0 +1,39 @@
+/*
+5 Phases of dom manuplulation:
+1.DOM 
+2.Selection of html elements
+3.How to change or manupulate html
+4.Css ko akise change karna hai
+5.Event listeners (events ko kaise suna jaye)
+*/
+
+// 2.
+// let heading = document.querySelector("h1")
+// console.log(heading)
+
+// let para = document.querySelector("p")
+// console.log(para)
+
+
+// 3.
+// let h1 = document.querySelector("h1");
+// h1.innerHTML = "hello Lucky"
+// console.log(h1)
+
+// let para = document.querySelector("p");
+// para.innerHTML = ""
+
+
+
+// let h1 = document.querySelector("h1");
+// h1.innerText = "My name is Lucky"
+// h1.style.color = "pink"
+// h1.style.backgroundColor = "purple"
+
+
+// let h1 = document.querySelector("h1")
+// h1.addEventListener('click',()=>{
+//     h1.style.backgroundColor = "green"
+//     h1.style.color = "white"
+//     h1.innerHTML = "L love you Lucky Roy"
+// })
